7866a98c9450d1fa40dd4d3dd5183c4862f06703
Riad Elamine <relamine@e3r1p13.1337.ma>
2025-07-26 01:21:55 +0100

    add new service my_website

---
+++ srcs/requirements/my_website/conf/www/script.js
@@ -0,0 +1,5 @@
+setTimeout(() => {
+  alert(">>> Welcome to My Containerized Stack <<<\n\n" +
+        "Docker | WordPress | Nginx | MariaDB | Redis\n" + 
+        "Explore my DevOps solutions");
+}, 500);
